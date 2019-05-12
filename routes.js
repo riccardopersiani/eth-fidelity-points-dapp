e42a87a0cd5622a99b2c18bfc6f9d13bc9be6f89
Riccardo Persiani <r.persiani92@gmail.com>
2019-05-12 17:26:26 +0100

    fix: routes update

--- routes.js
+++ routes.js
@@ -5,12 +5,11 @@ const routes = require('next-routes')();
 
 // New route mapping with add(), pattern that we look for and that the file
 routes
-    .add('/index', '/index')
-    .add('/start', '/start')
-    .add('/stats', '/stats')
-    .add('/shop', '/shop')
-    .add('/createTokens', '/createTokens')
-    .add('/newShop', '/newShop')
+    .add(`/index`, '/index')
+    .add(`/stats`, '/stats')
+    .add(`/shop`, '/shop')
+    .add(`/createTokens`, '/createTokens')
+    .add(`/newShop`, '/newShop')
     .add(`/shops/0xE82d7CD3186212819D152b6d27ac88762B147F55`, '/shops/0xE82d7CD3186212819D152b6d27ac88762B147F55')
     .add(`/shops/0x36C6BbF42E54a693320E725D46E4363Be6A9e338`, '/shops/0x36C6BbF42E54a693320E725D46E4363Be6A9e338')
     .add(`/auth/signIn`, '/auth/signIn')
@@ -26,4 +25,4 @@ routes
     .add(`/approve/approveShop`, '/approve/approveShop')
     .add(`/approve/approvePayment`, '/approve/approvePayment');
 
-module.exports = routes;
+  module.exports = routes;
